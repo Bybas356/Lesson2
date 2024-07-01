ad14fde7d808344771ed6e5ca8560d207862bfb6
Maxim <boss.mirkovskiy@gmail.com>
2024-07-01 14:22:04 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+a = 12
+b = 1.5
+Course_name = 'Python,'
+Time_for_one_task = b / a
+print('Курс:', Course_name, 'всего задач:' + str(a) + ',', 'затрачено часов:', str(b) + ',', 'среднее время выполнения', Time_for_one_task, 'часа.')
